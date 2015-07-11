55c71d11256add2f28f58bc6b6dd47a7e975b400
Antonio Brandao <antoniobrandaodesign@gmail.com>
2015-07-11 05:15:11 +0100

    Improvements and cleanup

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # ab-slideshow
 
-Hyper simple vanilla slideshow using background-image and dynamic creation/removal of dom elements..
+Hyper simple responsive vanilla slideshow using background-image instead of IMG and dynamic creation / removal of dom elements.
+
+Necessary arguments:
+
+- array of image URLs
+- reference to parent element which will hold the images
+
+This slideshow doesn't require markup. No need to add the images to the DOM before calling it.
+
+Just set targetElement to the element where you want your slideshow to be, and while the slideshow progresses, DOM elements will be automatically added and removed to display the images.
+
+
+## How it works internally
+
+IMG tags are not used. Instead, each slide element is a dynamically created DIV with the CSS property "background-image" set to "(url_of_image) no-repeat center center fixed" - and the CSS property "background-size" set to 'cover'. This is done to ensure perfect fitting of the image at any size of the parent element holding the slideshow. Handy for responsive design. You can resize the parent element as you wish, the images will always cover the available area in the best possible fit.
+
+No CSS is needed from you (except for the parent element). The slideshow and the slides automatically adopt the width and height of the parent element, which is the required parameter "targetElement". Please ensure the "targetElement" has the size you need it to have.
+
+When the slideshow starts, it creates a DIV in the given targetElement (using appendChild), with it's background-image property set to the URL of the image file, it's background-size property to 'cover' and opacity 1.
+
+Each time the slideshow transitions to a new image, a new DIV is created on top of the current one, with it's background-image set to the URL of the next image file, and with opacity 0. CSS transitions are then activaved in the new DIV and it's opacity set to 1, making the DIV fade in. When the new DIV finishes fading in, the previous DIV is removed from the DOM. 
+
+Roadmap:
+
+Previous / Next functionality
+Circles showing current position
+
 
 ## Install
 
@@ -12,6 +38,37 @@ $ npm install ab-slideshow --save-dev
 
 ## Usage
 
+	var slideshow = require("ab-slideshow");
+
+	// Basic usage
+
+	var new_slideshow = new slideshow({
+        images: ['path/img1.png', 'path/img2.jpg'],
+        targetElement: document.getElementById('slideshow-container')
+    });
+
+	// Advanced usage
+
+	var new_slideshow = new slideshow({
+        images: ['path/img1.png', 'path/img2.jpg'],
+        targetElement: document.getElementById('slideshow-container')
+        transitionDuration: 1.5, // in seconds
+        slideDuration: 4, // in seconds
+        autoStart: true, // if false, only first image is shown. then call slideshow.start()
+        onSlideChange: null, // callback for each slide change. returns current slide index
+        debug: true, // displays useful console.logs
+        startIndex: 0, // option to select initial slide index
+        classes: ['some-class'] // classes in this array will be added to each slide
+    });
+
+    // methods
+    slideshow.pause() - pauses the slideshow
+    slideshow.start() - starts / unpauses the slideshow
+    slideshow.destroy() - stops the slideshow looping and removes any added images from the DOM
+
+    // getters
+    slideshow.currentSlide - returns current slide index
+    slideshow.currentSlideElement - returns current slide element
 
 ## License
 
--- index.js
+++ index.js
@@ -12,103 +12,140 @@ function ABSlideshow(options)
         images              : [],
         targetElement       : null,
         transitionDuration  : 1.5, // in seconds
-        slideDuration 		: 10, // in seconds
-        showButtons 		: false,
-        autoStart 			: true,
-        parent              : null,
-        onReady             : null,
+        slideDuration       : 4, // in seconds
+        autoStart           : true,
+        active              : false,
         onSlideChange       : null,
-        debug               : true,
+        debug               : false,
         startIndex          : 0,
         classes             : [],
-        properties          : {},
+        // showButtons      : false, // maybe add this feature later
     };
 
-    if (this.debug) { console.log('::: ABSlideshow :::'); };
+    if (this.settings.debug) { console.log(' ::: ABSlideshow ::: '); };
 
     this.settings   = this.extend(this.settings, options);
-    this.debug      = this.settings.debug;
-    this.interval 	= {};
-    this.currentSlideElement = {};
-    this.currentIndex = this.settings.startIndex;
+    this.interval   = {};
 
-    if (this.settings.targetElement) {
-    	this.init();  	
-    } else {
-    	if (this.debug) { console.log('ABSlideshow ::: no targetElement provided, cannot init'); };
+    var ok_to_start = true;
+
+    if (!this.settings.targetElement) {
+        ok_to_start = false;
+        if (this.settings.debug) { console.log('ABSlideshow ::: no targetElement provided, cannot work'); };
     }
-}
 
-ABSlideshow.prototype.init = function init()
-{
-    if (this.debug) { console.log('ABSlideshow.prototype.init'); };
+    if (!this.settings.images || this.settings.images.length <= 0) {
+        ok_to_start = false;
+        if (this.settings.debug) { console.log('ABSlideshow ::: no images array provided, cannot work'); };
+    }
 
-    var new_element = this.prepareNewElementAtIndex(this.currentIndex, true);
-
-    this.settings.targetElement.appendChild(new_element);
-
-    this.currentSlideElement = new_element;
-
-	if (this.settings.autoStart) {
-		this.start();
-	};    
+    if (ok_to_start) { if (this.settings.autoStart) { this.start(); };
+    } else { console.log('ABSlideshow ::: ok_to_start false'); }
 }
 
 ABSlideshow.prototype.start = function start()
 {
-    if (this.debug) { console.log('ABSlideshow.prototype.start'); };
+    if (this.settings.debug) { console.log('ABSlideshow.prototype.start'); };
 
-    var self = this;
+    if (!this.settings.active)
+    {
+        this.settings.active = true;
+        
+        var self = this;
+        var removePreviousElement_delay = (this.settings.transitionDuration + 0.8) * 1000;
 
-    this.interval = setInterval(function()
-	{
-	    if (self.debug) { console.log('ABSlideshow.prototype.processInterval'); };
+        // ensure at least relative positioning in targetElement
+        var dastyle  = window.getComputedStyle(self.settings.targetElement);
+        var position = dastyle.getPropertyValue('position');
+        if (position !== 'absolute' && position !== 'relative' && position !== 'fixed') {
+            self.settings.targetElement.style.position = 'relative';
+        }
 
-	    self.currentIndex = self.currentIndex + 1;
-	    if (self.currentIndex === self.settings.images.length) { self.currentIndex = 0 };
+        var executeSlideshowLoop = function()
+        {
+            if (self.settings.debug) { console.log('ABSlideshow.prototype.processInterval'); };
 
-	    var new_element = self.prepareNewElementAtIndex(self.currentIndex);
+            if (self.settings.active) 
+            {
+                if (!self.settings.targetElement.firstChild) { // starting first time
 
-	    self.settings.targetElement.appendChild(new_element);
+                    if (self.settings.debug) { console.log('ABSlideshow.prototype.processInterval ::: starting looping') };
 
-	    var td = (self.settings.transitionDuration + 0.1) * 1000;
+                    // set currentIndex
+                    self.currentIndex = self.settings.startIndex;
 
-	    var old_slide = self.currentSlideElement;
+                    // prepare new slide
+                    var new_element = self.prepareNewElementAtIndex(self.currentIndex, true);
 
-	    self.currentSlideElement = new_element;
+                    // add it to the parent
+                    self.settings.targetElement.appendChild(new_element);
+                }
+                else // normal loop
+                {
+                    if (self.settings.debug) { console.log('ABSlideshow.prototype.processInterval ::: normal loop') };
 
-	    setTimeout(function()
-	    {
-	    	new_element.style.opacity = 1;
-	    }, 100);
-    	
-    	setTimeout(function()
-	    {
-	    	old_slide.parentNode.removeChild(old_slide);
-	    }, td);
-    
-	}, self.settings.slideDuration * 1000);
+                    // set currentIndex
+                    self.currentIndex = self.currentIndex + 1;
+                    if (self.currentIndex === self.settings.images.length) { self.currentIndex = 0 };
+
+                    // prepare new slide
+                    var new_element = self.prepareNewElementAtIndex(self.currentIndex);
+
+                    // add it to the parent
+                    self.settings.targetElement.appendChild(new_element);
+
+                    // remove previous after delay
+                    setTimeout(function() {
+                        self.settings.targetElement.firstChild.parentNode.removeChild(self.settings.targetElement.firstChild);
+                    }, removePreviousElement_delay);
+                }
+
+                if (self.settings.debug) { console.log('ABSlideshow.prototype.processInterval ::: self.currentIndex: ' + self.currentIndex); };
+                
+                // call new slide if slideshow is still active
+                setTimeout(function() {
+                    if (self.settings.active) {
+                        executeSlideshowLoop();
+                    };
+                }, removePreviousElement_delay + (self.settings.slideDuration * 1000));
+
+                setTimeout(function()
+                {
+                    if (self.settings.active) {
+                        // call onChange callback is given in parameters
+                        if (self.settings.onSlideChange) {
+                            self.settings.onSlideChange(selt.currentIndex);
+                        };
+
+                        // show new slide
+                        new_element.style.opacity = 1;
+
+                        self.currentSlideElement = new_element;
+                    };
+                }, 400);
+            };
+        };
+
+        executeSlideshowLoop();
+    }
 }
 
 ABSlideshow.prototype.pause = function pause()
 {
-    if (this.debug) { console.log('ABSlideshow.prototype.pause'); };
+    if (this.settings.debug) { console.log('ABSlideshow.prototype.pause'); };
 
-    clearInterval(this.interval);
+    this.settings.active = false;
 }
 
 ABSlideshow.prototype.destroy = function destroy()
 {
-    if (this.debug) { console.log('ABSlideshow.prototype.destroy'); };
+    if (this.settings.debug) { console.log('ABSlideshow.prototype.destroy'); };
 
-    clearInterval(this.interval);
+    this.settings.active = false;
     
-    if (this.currentSlideElement) {
-    	if (this.currentSlideElement.parentNode) {
-    		this.currentSlideElement.parentNode.removeChild(this.currentSlideElement);
-    	};
-    	this.currentSlideElement = null;
-    };
+    while (this.settings.targetElement.firstChild) {
+        this.settings.targetElement.removeChild(this.settings.targetElement.firstChild);
+    }
 }
 
 
@@ -124,58 +161,80 @@ ABSlideshow.prototype.destroy = function destroy()
 
 ABSlideshow.prototype.prepareNewElementAtIndex = function prepareNewElementAtIndex(index, visible)
 {
-    if (this.debug) { console.log('ABSlideshow.prototype.prepareNewElementAtIndex'); };
+    if (this.settings.debug) { console.log('ABSlideshow.prototype.prepareNewElementAtIndex'); };
 
     var imageURLAtIndex = this.settings.images[index];
 
     var new_element = document.createElement('DIV');
 
-    new_element.style.position 				= 'absolute';
-    new_element.style.top 					= '0px';
-    new_element.style.left 					= '0px';
-    new_element.style.height 				= this.settings.targetElement.clientHeight + 'px';
-    new_element.style.width 				= this.settings.targetElement.clientWidth  + 'px';
-    new_element.style.background 			= 'url(' + imageURLAtIndex + ') no-repeat center center fixed'
-	new_element.style.webkitBackgroundSize 	= 'cover';
-	new_element.style.mozBackgroundSize 	= 'cover';
-	new_element.style.oBackgroundSize 		= 'cover';
-	new_element.style.backgroundSize 		= 'cover';
+    new_element.style.overflow                  = 'hidden';
+    new_element.style.position                  = 'absolute';
+    new_element.style.top                       = '0px';
+    new_element.style.left                      = '0px';
+    new_element.style.height                    = '100%';
+    new_element.style.width                     = '100%';
+    new_element.style.background                = 'url(' + imageURLAtIndex + ') no-repeat center center fixed'
+    new_element.style.webkitBackgroundSize      = 'cover';
+    new_element.style.mozBackgroundSize         = 'cover';
+    new_element.style.oBackgroundSize           = 'cover';
+    new_element.style.backgroundSize            = 'cover';
+    new_element.style.webkitTransform           = 'translate3d(0, 0, 0.00001)';
+    new_element.style.mozTransform              = 'translate3d(0, 0, 0.00001)';
+    new_element.style.msTransform               = 'translate3d(0, 0, 0.00001)';
+    new_element.style.oTransform                = 'translate3d(0, 0, 0.00001)';
+    new_element.style.transform                 = 'translate3d(0, 0, 0.00001)';
+    new_element.style.webkitBackfaceVisibility  = 'hidden';
+    new_element.style.mozBackfaceVisibility     = 'hidden';
+    new_element.style.msBackfaceVisibility      = 'hidden';
+    new_element.style.oBackfaceVisibility       = 'hidden';
+    new_element.style.backfaceVisibility        = 'hidden';
+    new_element.style.webkitPerspective         = '1000';
+    new_element.style.mozPerspective            = '1000';
+    new_element.style.msPerspective             = '1000';
+    new_element.style.oPerspective              = '1000';
+    new_element.style.Perspective               = '1000';
 
-	if (visible) {  new_element.style.opacity = 1; console.log('1');
-	} else 		 {  new_element.style.opacity = 0; console.log('2');
-	}
+    if (visible) {  new_element.style.opacity = 1;
+    } else       {  new_element.style.opacity = 0;
+    }
 
-	// add css transition for fading
-    var transition_value 					= 'all ' + this.settings.transitionDuration + 's ease-out';
-    new_element.style.webkitTransition   	= transition_value;
-    new_element.style.mozTransition      	= transition_value;
-    new_element.style.msTransition       	= transition_value;
-    new_element.style.oTransition        	= transition_value;
+    // add css transition for fading
+    var transition_value                    = 'all ' + this.settings.transitionDuration + 's ease-out';
+    new_element.style.webkitTransition      = transition_value;
+    new_element.style.mozTransition         = transition_value;
+    new_element.style.msTransition          = transition_value;
+    new_element.style.oTransition           = transition_value;
 
-	return new_element;
+    // add any given classes to the new slide element
+    for (var i = this.settings.classes.length - 1; i >= 0; i--) {
+        if (i === 0) { new_element.className = new_element.className + this.settings.classes[i] } 
+        else { new_element.className = new_element.className + ' ' + this.settings.classes[i] };
+    };
+
+    return new_element;
 }
 
 
 ABSlideshow.prototype.extend = function extend( defaults, options ) 
 {
-	var extended = {};
-	var prop;
+    var extended = {};
+    var prop;
 
-	for (prop in defaults) 
-	{
-		if (Object.prototype.hasOwnProperty.call(defaults, prop)) {
-			extended[prop] = defaults[prop];
-		}
-	}
+    for (prop in defaults) 
+    {
+        if (Object.prototype.hasOwnProperty.call(defaults, prop)) {
+            extended[prop] = defaults[prop];
+        }
+    }
 
-	for (prop in options) 
-	{
-		if (Object.prototype.hasOwnProperty.call(options, prop)) 
-		{
-			extended[prop] = options[prop];
-		}
-	}
-	return extended;
+    for (prop in options) 
+    {
+        if (Object.prototype.hasOwnProperty.call(options, prop)) 
+        {
+            extended[prop] = options[prop];
+        }
+    }
+    return extended;
 };
 
 module.exports = ABSlideshow;
